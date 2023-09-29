e1780765af179c1830379baaff785bcf3635d4ec
Margulis162 <markmargulis@proton.me>
2023-09-28 21:01:24 -0400

    initial and class constractor

---
+++ json.js
@@ -0,0 +1,22 @@
+// vars and classes 
+
+class Cultist {
+    first_name;
+    last_name;
+    age;
+    gender;
+    phone;
+    email;
+    additional_data;
+
+    constructor(first_name, last_name, age, gender, phone, email, additional_data,){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+        this.additional_data = additional_data;
+    }
+}
+
